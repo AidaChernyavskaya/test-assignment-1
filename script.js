f2ddebf196c08892641e9b1d15ac7375b9cf2033
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-09-08 11:56:04 +0300

    Add brands page block

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ const countVisibleSlides = (pageBlock = '') => {
             } else {
                 return 4;
             }
-            return window.innerWidth > 850 ? 6 : 4;
+        case 'brands':
+            if (window.innerWidth < 1300 && window.innerWidth > 1150) {
+                return 7;
+            } else if (window.innerWidth < 1150 && window.innerWidth > 600) {
+                return 5;
+            } else if (window.innerWidth < 600) {
+                return 3;
+            }
         default:
             return window.innerWidth > 850 ? 6 : 3;
     }
@@ -20,17 +27,7 @@ const toggleActiveClass = (slides, to, from) => {
     });
 };
 
-const digitsSlider = () => {
-    const arrowPrev = document.getElementById('digits_prev');
-    const arrowNext = document.getElementById('digits_next');
-    const slides = document.querySelectorAll('.digit');
-
-    let visible = countVisibleSlides('digits');
-    let from = 0;
-    let to = visible - 1;
-
-    toggleActiveClass(slides, to, from);
-
+const slider = (arrowPrev, arrowNext, from, to, slides) => {
     arrowPrev.addEventListener('click', () => {
         if (from - 1 >= 0) {
             arrowNext.classList.remove('arrow__disactive');
@@ -52,11 +49,38 @@ const digitsSlider = () => {
             arrowNext.classList.add('arrow__disactive');
         }
     });
+}
 
+const digitsSlider = () => {
+    const arrowPrev = document.getElementById('digits_prev');
+    const arrowNext = document.getElementById('digits_next');
+    const slides = document.querySelectorAll('.digit');
+
+    let visible = countVisibleSlides('digits');
+    let from = 0;
+    let to = visible - 1;
+
+    toggleActiveClass(slides, to, from);
+    slider(arrowPrev, arrowNext, from, to, slides);
 };
 
+const brandsSlider = () => {
+    const arrowPrev = document.getElementById('brands_prev');
+    const arrowNext = document.getElementById('brands_next');
+    const slides = document.querySelectorAll('.brand');
+
+    let visible = countVisibleSlides('brands');
+    let from = 0;
+    let to = visible - 1;
+
+    toggleActiveClass(slides, to, from);
+    slider(arrowPrev, arrowNext, from, to, slides);
+}
+
 digitsSlider();
+brandsSlider();
 
 window.addEventListener('resize', () => {
     digitsSlider();
+    brandsSlider();
 });
